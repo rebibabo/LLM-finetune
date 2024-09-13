22ea41751e26511f45012094a0e1b6f26945a1f7
rebibabo <2841329980@qq.com>
2024-09-13 11:35:14 +0800

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 echo 'export HF_HOME="/root/autodl-tmp/huggingface"' >> ~/.bashrc
 source ~/.bashrc
 pip install --upgrade pip
-pip install python-dotenv bitsandbytes transformers xformers trl peft
+pip install python-dotenv
 pip install "unsloth[cu121-torch240] @ git+https://github.com/unslothai/unsloth.git"
-pip install -r requirements.txt
-wget https://download1323.mediafire.com/9p7au1kucr6gLIVo2S6unekwLMfnBZXOHtPaWQ_xZ_v5uaMTVitV3cfbpoKA7VUaeVOZygsRWpdTnileTeNoDQf9HtEA9l3za7fUnN3sYji2foE4fFe-NB6T3YiRH6Dgy7BNxdZtSF3_8kpNBGiLiFp9Er-_QA0QDScM9olAYFw/no0bi7l46u3k5k2/unified_chip2.jsonl.gz
