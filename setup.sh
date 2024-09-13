03432547c56148a1b95ab3048bc8c006213075de
rebibabo <2841329980@qq.com>
2024-09-13 11:35:34 +0800

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-echo 'export HF_HOME="/root/autodl-tmp/huggingface"' >> ~/.bashrc
-source ~/.bashrc
 pip install --upgrade pip
 pip install python-dotenv
 pip install "unsloth[cu121-torch240] @ git+https://github.com/unslothai/unsloth.git"
